75500663181716c941ffe5635266600066f38cd6
Pontus-Nygren <Nygreon@10.11.12.129>
2016-11-09 13:32:52 +0100

    JAJA2

--- src/testgit/TestGit.java
+++ src/testgit/TestGit.java
@@ -17,7 +17,7 @@ public class TestGit {
     public static void main(String[] args) {
         // TODO code application logic here
         
-        
+        System.out.println("HEJ");
         System.out.println("CHANGES NUMERO UNO");
     }
     
