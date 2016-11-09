a86eb78cbaa4575cc19aac3f534eaaf477abf175
Allan <Allan@10.11.12.132>
2016-11-09 13:54:47 +0100

    mangemnage bujje

--- src/testgit/TestGit.java
+++ src/testgit/TestGit.java
@@ -15,7 +15,7 @@ public class TestGit {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Magnus har clonat Master, och nu på MangnusMangeBujhjje till mangeBujje");
     }
     
 }
